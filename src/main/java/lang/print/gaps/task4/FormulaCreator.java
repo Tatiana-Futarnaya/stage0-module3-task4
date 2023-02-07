e7d896ba463f073e3877e744401d39173ef76680
Tatiana-Futarnaya <tannia93@mail.ru>
2023-02-07 12:48:32 +0300

    third commit

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-       int a ,b;
-        System.out.println("(9*(a^2)-5*b+2+a-7)*((a+b-4*a*b)/2)");
+        char a ,b;
+        System.out.println("(9 * (a ^ 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2)");
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,14 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        int c='c';
-        int h='h';
-        int a='a';
-        int r='r';
-        System.out.println(c);
-        System.out.println(h);
-        System.out.println(a);
-        System.out.println(r);
+        System.out.println(99);
+        System.out.println(104);
+        System.out.println(97);
+        System.out.println(114);
 
     }
 }
